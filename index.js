c3674a6d2686ce800f8c83276f72f146f453a97b
tolentinoel <rckrleyn@gmail.com>
2020-11-15 12:42:22 -0800

    commit tree c3612c4036b07822f5168705b09592bbd7f15a02

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(names, word) {
+    let arrayOfmessages = [];
+    for (let i = 0; i < names.length; i++){
+        arrayOfmessages.push(`Thank you, ${names[i]}, for the wonderful ${word} gift!`);
+    }
+    return arrayOfmessages;
+}
+
+function countDown(num) {
+    while (num > 0) {
+        console.log(num);
+        num -= 1;
+    }
+    console.log(num);
+}
